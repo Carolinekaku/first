d912f1efc509f6636a51a0287baa901aea142934
Carolinekaku <caroline20126862@gmail.com>
2020-09-26 15:45:00 +0800

    add again

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
    public static void main (String [] args)
    {
 
-    int i = 10;	
+    int i = 10;	//test
     
     int j = 200; //add
    System.out.println("Hello");
